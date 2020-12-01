6850ad3f30919db1ed099fa8ece290825389bbb1
Tasha Lane <designtmg77@gmail.com>
2020-12-01 10:30:10 -0600

    recursive range

---
+++ Reverse/reverse.js
@@ -0,0 +1,25 @@
+function reverse(str) {
+    // add whatever parameters you deem necessary - good luck!
+    array = str.split("")
+    reverseStr = []
+    function reverseArray(arr) {
+        // 1. Base case
+        if (arr.length === 0) return
+        // 2. Recursive call
+        reverseStr.unshift(arr[0])
+        return reverseArray(arr.slice(1))
+    }
+    // 2.5 Call helper function
+    reverseArray(array)
+    // 3. Join reversed array and return the result
+    return reverseStr.join("")
+}
+
+function reverse2(str) {
+    if (str.length === 0) return ""
+    return str[str.length - 1] + reverse2(str.substring(0, str.length - 1))
+    // Udemy answer: return reverse2(str.slice(1)) + str[0])
+}
+
+console.log(reverse2('awesome')) // 'emosewa'
+console.log(reverse2('middleschool')) // 'loohcselddim'
